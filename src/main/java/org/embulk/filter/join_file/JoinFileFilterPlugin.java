3919d9d50d28a81007c9ead250d57278918c7ebe
Civitaspo <civitaspo@gmail.com>
2017-08-21 21:10:14 +0900

    Add JoinPageOutput Skelton

--- src/main/java/org/embulk/filter/join_file/JoinFileFilterPlugin.java
+++ src/main/java/org/embulk/filter/join_file/JoinFileFilterPlugin.java
@@ -2,6 +2,7 @@ package org.embulk.filter.join_file;
 
 import org.embulk.config.ConfigSource;
 import org.embulk.config.TaskSource;
+import org.embulk.filter.join_file.join.JoinPageOutput;
 import org.embulk.spi.FilterPlugin;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.Schema;
@@ -25,8 +26,6 @@ public class JoinFileFilterPlugin
             Schema outputSchema, PageOutput output)
     {
         PluginTask task = PluginTask.loadTask(taskSource);
-
-        // Write your code here :)
-        throw new UnsupportedOperationException("JoinFileFilterPlugin.open method is not implemented yet");
+        return new JoinPageOutput();
     }
 }
---
+++ src/main/java/org/embulk/filter/join_file/join/JoinPageOutput.java
@@ -0,0 +1,26 @@
+package org.embulk.filter.join_file.join;
+
+import org.embulk.spi.Page;
+import org.embulk.spi.PageOutput;
+
+public class JoinPageOutput
+        implements PageOutput
+{
+    @Override
+    public void add(Page page)
+    {
+
+    }
+
+    @Override
+    public void finish()
+    {
+
+    }
+
+    @Override
+    public void close()
+    {
+
+    }
+}
