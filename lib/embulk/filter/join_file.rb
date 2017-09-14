f136f597570af1b84ebff793985bf3ccf73827fa
Civitaspo <civitaspo@gmail.com>
2017-09-14 11:05:24 +0900

    Register InternalForwardOutputPlugin

--- lib/embulk/filter/join_file.rb
+++ lib/embulk/filter/join_file.rb
@@ -1,3 +1,7 @@
 Embulk::JavaPlugin.register_filter(
   "join_file", "org.embulk.filter.join_file.JoinFileFilterPlugin",
   File.expand_path('../../../../classpath', __FILE__))
+
+Embulk::JavaPlugin.register_output(
+    "internal_forward", "org.embulk.filter.join_file.plugin.InternalForwardOutputPlugin",
+    File.expand_path('../../../../classpath', __FILE__))
